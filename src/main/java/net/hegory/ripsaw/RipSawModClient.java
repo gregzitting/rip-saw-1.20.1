b0bec017e0c8ac4aaeff778712fedf7c483ebfcd
Hegory <greglzitting@gmail.com>
2024-12-21 19:10:53 -0700

    Initial Commit

---
+++ src/main/java/net/hegory/ripsaw/RipSawModClient.java
@@ -0,0 +1,10 @@
+package net.hegory.ripsaw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RipSawModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
